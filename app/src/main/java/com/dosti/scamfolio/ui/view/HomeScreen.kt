068f64ab7ca78713353d2cf4a9c4e86a0924b026
zeroParanoie <andrea.tsilogiannis@gmail.com>
2024-06-15 19:44:27 +0200

    starting horizontal view

--- app/src/main/java/com/dosti/scamfolio/ui/view/HomeScreen.kt
+++ app/src/main/java/com/dosti/scamfolio/ui/view/HomeScreen.kt
@@ -1,19 +1,10 @@
 package com.dosti.scamfolio.ui.view
 
 import android.annotation.SuppressLint
-import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
@@ -24,7 +15,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
--- app/src/main/java/com/dosti/scamfolio/ui/view/HomepageView.kt
+++ app/src/main/java/com/dosti/scamfolio/ui/view/HomepageView.kt
@@ -1,11 +1,12 @@
 package com.dosti.scamfolio.ui.view
 
-import androidx.compose.foundation.background
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,23 +14,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.dosti.scamfolio.R
-import com.dosti.scamfolio.SharedPrefRepository
 import com.dosti.scamfolio.db.entities.Purchasing
 import com.dosti.scamfolio.ui.chart.PieChartBalance
 import com.dosti.scamfolio.ui.theme.custom
@@ -38,6 +37,81 @@ import com.dosti.scamfolio.viewModel.HomepageViewModel
 
 @Composable
 fun Welcome(
+    viewModel: HomepageViewModel
+) {
+    val configuration = LocalConfiguration.current
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_PORTRAIT -> {
+            WelcomePortraitLayout(viewModel = viewModel)
+        }
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            WelcomeLandscapeLayout(viewModel = viewModel)
+        }
+        Configuration.ORIENTATION_UNDEFINED -> {
+            WelcomePortraitLayout(viewModel = viewModel)
+        }
+    }
+}
+
+@Composable
+fun WelcomeLandscapeLayout(
+    viewModel: HomepageViewModel
+) {
+    val username=viewModel.username
+    val balance = viewModel.balance.collectAsState().value
+    val transactions= viewModel.transactions.collectAsState().value
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.Top,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(0.4f)
+        ) {
+            TopLabel(username = username)
+            Spacer(modifier = Modifier.height(10.dp))
+            Card(modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    PieChartBalance(viewModel)
+
+                    BalanceText(balance = viewModel.roundDouble(balance).toString())
+                }
+            }
+        }
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Spacer(modifier=Modifier.height(10.dp))
+            Text(
+                text = "Transactions: ",
+                fontSize = 25.sp,
+                fontFamily = custom,
+                color = Color.White
+            )
+            Transactions(transactions,viewModel)
+        }
+    }
+}
+
+@Composable
+fun WelcomePortraitLayout(
     viewModel: HomepageViewModel,
 ) {
 
@@ -79,6 +153,7 @@ fun Welcome(
         Transactions(transactions,viewModel)
     }
 }
+
 
 @Composable
 fun TopLabel(
